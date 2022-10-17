feba5fd9d1343f32f593d3ce66155878f1e33458
antoni0caldeira <antoniocaldeira@yahoo.com>
2022-10-17 18:04:00 +0100

    Add Args.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var x = 31.5;
+var y = 2.5;
+var result = x + y;
+var greeting = "Tenho um pichalhão";
+
+
+Console.WriteLine($"{args[0]}, {greeting} {result}");
+
